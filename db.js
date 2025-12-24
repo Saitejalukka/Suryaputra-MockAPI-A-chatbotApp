1fe19cc025c34f29e01f2cb09c9dd05ba9a8e93d
Saitejalukka <lukkasaiteja2002@gmail.com>
2025-12-24 12:08:36 +0530

    updated db.js

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
-const url = "mongodb://127.0.0.1:27017";
 import { MongoClient } from "mongodb";
 
 let db;
 
 export const connectDB = async () => {
 	try {
-		const client = new MongoClient(process.env.MONGODB_URI);
-		await client.connect();
+		const client = new MongoClient(process.env.MONGODB_URI, {
+			tls: true,
+			tlsAllowInvalidCertificates: false,
+		});
 
+		await client.connect();
 		db = client.db(process.env.DB_NAME);
-		console.log("✅ MongoDB connected successfully");
+
+		console.log("✅ MongoDB Atlas connected");
 	} catch (error) {
 		console.error("❌ MongoDB connection failed:", error.message);
 		process.exit(1);
@@ -17,8 +20,6 @@ export const connectDB = async () => {
 };
 
 export const getDB = () => {
-	if (!db) {
-		throw new Error("Database not initialized");
-	}
+	if (!db) throw new Error("Database not initialized");
 	return db;
 };
